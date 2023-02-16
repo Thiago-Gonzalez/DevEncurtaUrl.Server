6f54fd1465729b96edaa2fb87b458ce2ded8272e
Thiago González <thiagogonzalez2000@gmail.com>
2023-02-16 20:40:58 -0300

    Add EF Core packages and services to container and create dbcontext

--- DevEncurtaUrl.API/Program.cs
+++ DevEncurtaUrl.API/Program.cs
@@ -1,8 +1,21 @@
+using DevEncurtaUrl.Application.Commands.AddShortenedLinkCommand;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(
+        policy => {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        }
+    );
+});
+
+builder.Services.AddMediatR(typeof(AddShortenedLinkCommand));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
